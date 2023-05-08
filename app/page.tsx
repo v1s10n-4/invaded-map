e4d40a0834999d21faa3c6b457cca9ab69cae83b
v1s10n_4 <romain.daniel@epitech.eu>
2023-05-08 16:46:59 +0200

    build(assets): allow to use svg as components or static image

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import File from "pixelarticons/svg/file.svg";
+import Fesse from "@/public/vercel.svg?url";
+import React from 'react';
 
 export default function Home() {
   return (
@@ -6,6 +9,7 @@ export default function Home() {
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
+          <File className="text-sky-400 w-5 h-5"/>
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
@@ -17,7 +21,7 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src="/vercel.svg"
+              src={Fesse}
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
@@ -27,11 +31,10 @@ export default function Home() {
           </a>
         </div>
       </div>
-
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
+          src={Fesse}
           alt="Next.js Logo"
           width={180}
           height={37}
