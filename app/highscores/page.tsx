a7b98b38676cd2a768db44e6fb8466d1d3ba6bf8
v1s10n_4 <romain.daniel@epitech.eu>
2023-05-08 18:17:39 +0200

    feat: init highscores page

---
+++ app/highscores/page.tsx
@@ -0,0 +1,26 @@
+import { HighScoresResponse } from '@/types/SIAPI';
+
+const SIAPI = {
+  highscores: "https://space-invaders.com/api/highscore/?uid=false"
+}
+const getHightScores: () => Promise<HighScoresResponse> = async () => {
+  const res = await fetch(SIAPI.highscores);
+  return await res.json();
+}
+const HightScorePage = async () => {
+  const highscores = await getHightScores();
+  return (
+    <main className="bg-black text-white flex justify-center">
+      <ul className="flex flex-col gap-1">
+        {highscores.Players.map(entry => (
+          <li key={entry.rank} className="border p-2">
+            <h4>#{entry.rank}: {entry.name}</h4>
+            <p>Score: <span>{entry.score}</span>, Invaders flashed: <span>{entry.invaders_count}</span><br/>Shots: <span>{entry.nbShots}</span>, Cities discovered: <span>{entry.city_count}</span></p>
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
+}
+
+export default HightScorePage;
