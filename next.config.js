148130b683fe17259f6401aa49563e9a11bde5ca
v1s10n_4 <romain.daniel@epitech.eu>
2023-05-22 00:12:45 +0200

    chore: add temp redirection for root path

--- next.config.js
+++ next.config.js
@@ -1,33 +1,44 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    experimental: {
-        typedRoutes: true,
-        serverActions: true,
-    },
-    webpack(config) {
-        const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
-        config.module.rules.push(
-            {
-                ...fileLoaderRule,
-                test: /\.svg$/i,
-                resourceQuery: /url/,
+  reactStrictMode: true,
+  experimental: {
+    typedRoutes: true,
+    serverActions: true,
+  },
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    );
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        resourceQuery: { not: /url/ },
+        use: [
+          {
+            loader: "@svgr/webpack",
+            options: {
+              icon: true,
             },
-            {
-                test: /\.svg$/i,
-                resourceQuery: { not: /url/ },
-                use: [{
-                    loader: '@svgr/webpack',
-                    options: {
-                        icon: true
-                    }
-                }],
-            },
-        );
-        fileLoaderRule.exclude = /\.svg$/i;
+          },
+        ],
+      }
+    );
+    fileLoaderRule.exclude = /\.svg$/i;
 
-        return config;
+    return config;
+  },
+  redirects: async () => [
+    {
+      source: "/",
+      destination: "/map",
+      permanent: false,
     },
-}
+  ],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
