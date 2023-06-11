fcc5b975ba6275bb4cc1191dea90e9d506dbb65c
v1s10n_4 <romain.daniel@epitech.eu>
2023-06-11 19:22:28 +0200

    feat: use proper metadata base

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,11 @@ const sixtyfour = localFont({
 const appName = "Invaded Map";
 const appDescription =
   "Locate all space invaders for Flash Invaders app & more!";
-const appUrl = "https://invaded-map.com";
+const appUrl = new URL(
+  process.env.VERCEL_URL || process.env.URL || "http://localhost:3000"
+);
 export const metadata: Metadata = {
+  metadataBase: appUrl,
   title: appName,
   description: appDescription,
   applicationName: appName,
