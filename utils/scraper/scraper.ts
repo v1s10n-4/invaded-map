c03bebcf5a797031df4b76c2f78ab314c9d80509
v1s10n-4 <romain.daniel@epitech.eu>
2024-03-17 01:59:13 +0100

    fix: better scraper that avoid getting banned

--- utils/scraper/scraper.ts
+++ utils/scraper/scraper.ts
@@ -1,29 +1,37 @@
 import invaders from "./awaz.json";
 import { parse } from "node-html-parser";
-const rootUrl = "https://www.awazleon.space";
 
 type Invader = (typeof invaders)[0];
+
+const rootUrl = "https://www.awazleon.space";
+
+const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time));
+
 const getImages = async (inv: Invader) => {
   console.log(`fetching ${inv.name}`);
   const res = await fetch("https://www.awazleon.space/A_buildmodal.php", {
     headers: {
       "accept": "*/*",
-      "accept-language": "en-US,en;q=0.9",
+      "accept-language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
+      "cache-control": "no-cache",
       "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-      "sec-ch-ua": '"Not(A:Brand";v="24", "Chromium";v="122"',
+      "pragma": "no-cache",
+      "sec-ch-ua": "\"Chromium\";v=\"122\", \"Not(A:Brand\";v=\"24\", \"Google Chrome\";v=\"122\"",
       "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": '"macOS"',
+      "sec-ch-ua-platform": "\"macOS\"",
       "sec-fetch-dest": "empty",
       "sec-fetch-mode": "cors",
       "sec-fetch-site": "same-origin",
       "x-requested-with": "XMLHttpRequest",
-      "cookie": "PHPSESSID=ec1f069708d464a5e31f2ee2daae6567",
+      "cookie": "PHPSESSID=ae40a01cc1ad26216705c565bca50932",
       "Referer": "https://www.awazleon.space/bySI.php",
-      "Referrer-Policy": "strict-origin-when-cross-origin",
+      "Referrer-Policy": "strict-origin-when-cross-origin"
     },
     body: `p_ref=${inv.name}&cityInv=${inv.cityName}`,
     method: "POST",
   });
+
+  if (res.status !== 200) throw Error(`fesse! [${res.status}] ${res.statusText}`);
 
   const body = await res.text();
 
@@ -32,22 +40,25 @@ const getImages = async (inv: Invader) => {
   const comment =
     root.querySelector(".d-flex .justify-content-center .mt-1")?.innerText ||
     null;
-  const images = elements.map((el) => {
-    return {
+  const images = elements.map((el) => ({
       url: rootUrl + el.querySelector("img")?.attrs.src,
       author: el.querySelector(".bottom-left")?.innerText.split(" ")[1],
-    };
-  });
+    }));
   return {
     images,
     comment,
   };
 };
 
-// @ts-ignore
-const newInvadersList = await Promise.all(
-  invaders.map(async (i) => ({ ...i, ...(await getImages(i)) }))
-);
+const newInvadersList = [];
+
+const randomInRange = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+for (let inv of invaders) {
+  newInvadersList.push({...inv, ...(await getImages(inv))});
+  await sleep(randomInRange(150, 250));
+}
+
 console.log(newInvadersList);
 // @ts-ignore
 const bytes = await Bun.write(
