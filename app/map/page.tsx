6d911a5a511e2590f4a74baf86aed0f675eb01b1
v1s10n_4 <romain.daniel@epitech.eu>
2024-03-21 14:46:07 +0100

    fix: thumbnails on /map

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -16,7 +16,7 @@ const MapPage: FC = () => {
   }, []);
   return (
     <div
-      className={`carousel-vertical m-0.5 grid h-[calc(100%-4px)] grid-cols-2 gap-0.5 scrollbar md:grid-cols-3 lg:grid-cols-4`}
+      className={`scrollbar carousel-vertical m-0.5 grid h-[calc(100%-4px)] grid-cols-2 gap-0.5 md:grid-cols-3 lg:grid-cols-4`}
     >
       {isHydrated &&
         invaders.slice(0, 20).map((invader) => (
@@ -31,7 +31,7 @@ const MapPage: FC = () => {
           >
             <Image
               className="h-fit w-full object-contain"
-              src={`/assets/images/invaders/${invader.name}.png`}
+              src={invader.thumbnail}
               alt={`${invader.name}'s invader picture`}
               width={192}
               height={192}
