c1eea450d2204b2938adc97d7fc0633e635b5643
v1s10n_4 <romain.daniel@epitech.eu>
2024-03-22 03:44:31 +0100

    fix(tricky): sharp unsupported on edge to convert avif to png

---
+++ app/api/get-thumbnail/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest } from "next/server";
+import sharp from "sharp";
+
+export const runtime = "nodejs";
+export async function GET(request: NextRequest): Promise<Response> {
+  const param = request.nextUrl.searchParams.get("url");
+  if (!param) return Response.json({ error: true, data: null });
+  try {
+    const url = new URL(param);
+    const res = await fetch(url);
+    const buffer = await res.arrayBuffer();
+    const thumbnailBuffer = await sharp(buffer).toFormat("png").toBuffer();
+    const image = thumbnailBuffer.toString("base64");
+    return Response.json(image);
+  } catch (err) {
+    console.error(err);
+    return Response.json({ error: true, data: null });
+  }
+}
