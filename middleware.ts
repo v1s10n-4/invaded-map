6a9becbfabc6a308418ceb9859b98b772b2b24e9
v1s10n_4 <romain.daniel@epitech.eu>
2024-03-23 03:49:41 +0100

    fix: allow og image api routes to be fetched from outside

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
-  matcher: "/api/:path*",
+  matcher: "/(api(?!/invaders/[^/]+/OG).*)",
 };
 
 export function middleware(request: NextRequest) {
