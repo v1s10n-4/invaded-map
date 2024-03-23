58d2bc8b6eeeea4e0332186b9d2cbc5c0f43d197
v1s10n_4 <romain.daniel@epitech.eu>
2024-03-23 03:53:14 +0100

    fix: add authorization headers for api data fetching functions

--- utils/data.ts
+++ utils/data.ts
@@ -1,4 +1,4 @@
-import { Invader, InvaderState } from "@/db";
+import { Invader, InvaderState, InvaderWithLocation } from "@/db";
 
 export const getState = (state: InvaderState) =>
   ({
@@ -10,13 +10,41 @@ export const getState = (state: InvaderState) =>
     U: "Unknown",
   })[state];
 
+const headers: HeadersInit = {
+  "api-token": process.env.API_TOKEN!,
+};
 export const getInvader = async (invaderName: string) => {
   const route = `/api/invaders/${invaderName}`;
   const res = await fetch(route, {
+    headers,
     next: {
       tags: [route],
     },
   });
   const invader: Invader | undefined = await res.json();
   return invader;
+};
+
+export const getInvadersWithLocation = async () => {
+  const route = "/api/map/invaders";
+  const response = await fetch(route, {
+    headers,
+    next: {
+      tags: [route],
+    },
+  });
+  const invaders: InvaderWithLocation[] = await response.json();
+  return invaders;
+};
+
+export const getInvaders = async () => {
+  const route = "/api/invaders";
+  const response = await fetch(route, {
+    headers,
+    next: {
+      tags: [route],
+    },
+  });
+  const invaders: Invader[] = await response.json();
+  return invaders;
 };
