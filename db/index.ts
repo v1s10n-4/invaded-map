4f7c1ccc29e1d1eb30313fa5dda13499171ad2d0
v1s10n_4 <romain.daniel@epitech.eu>
2024-03-24 05:23:46 +0100

    feat: hacky way to make vercel postgres work on local db

--- db/index.ts
+++ db/index.ts
@@ -1,7 +1,17 @@
 import { invaders } from "@/db/schema/invaders";
-import { sql } from "@vercel/postgres";
+import { neonConfig } from "@neondatabase/serverless";
+import { sql, createPool, createClient } from "@vercel/postgres";
 import { InferInsertModel, InferSelectModel } from "drizzle-orm";
-import { drizzle } from "drizzle-orm/vercel-postgres";
+import {
+  drizzle as LocalDrizzle,
+  type NodePgDatabase,
+} from "drizzle-orm/node-postgres";
+import {
+  drizzle as VercelDrizzle,
+  type VercelPgDatabase,
+} from "drizzle-orm/vercel-postgres";
+import { drizzle as NeonDrizzle } from "drizzle-orm/neon-serverless";
+import { Pool } from "pg";
 
 type InvaderTable = typeof invaders;
 export type NewInvader = InferInsertModel<InvaderTable>;
@@ -10,5 +20,40 @@ export type InvaderState = Invader["state"];
 export type InvaderWithLocation = Invader & {
   location: NonNullable<Invader["location"]>;
 };
+// let db:
+//   | VercelPgDatabase<Record<string, never>>
+//   | NodePgDatabase<Record<string, never>>;
+// console.log({
+//   LOCAL_env: process.env.LOCAL,
+//   cond: process.env.LOCAL === "true",
+// });
+// if (process.env.LOCAL === "true") {
+//   const client = new Pool({
+//     connectionString: process.env.POSTGRES_URL!,
+//   });
+//   await client.connect();
+//   db = LocalDrizzle(client);
+// } else {
 
-export const db = drizzle(sql);
+// const db = VercelDrizzle(sql);
+
+// neonConfig.wsProxy = (host) => {
+//   console.log(host);
+//   return `${host}:5433/v1`;
+// };
+// // Disable all authentication and encryption
+// neonConfig.useSecureWebSocket = false;
+// neonConfig.pipelineTLS = false;
+// neonConfig.pipelineConnect = false;
+// const db = VercelDrizzle(sql);
+const client = createClient();
+client.neonConfig.wsProxy = (host) => `${host}:5433/v1`;
+// Disable all authentication and encryption.
+client.neonConfig.useSecureWebSocket = false;
+client.neonConfig.pipelineTLS = false;
+client.neonConfig.pipelineConnect = false;
+const db = VercelDrizzle(client);
+db.session.client.connect();
+// }
+
+export { db };
