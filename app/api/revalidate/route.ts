0da8aa249cacd00861e94ff83c0b5bd5d55a0b85
v1s10n_4 <romain.daniel@epitech.eu>
2024-03-28 08:12:42 +0100

    fix(revalidation): prevent path query param check to be always true

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -8,7 +8,7 @@ export async function GET(request: NextRequest) {
   }
 
   const paths = request.nextUrl.searchParams.getAll("path");
-  if (paths) {
+  if (paths.length) {
     for (const path of paths) {
       revalidatePath(path);
     }
