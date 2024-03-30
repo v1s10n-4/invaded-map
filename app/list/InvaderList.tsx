73e7f67982ebe41a1faf349caf164c623a6b8765
v1s10n_4 <romain.daniel@epitech.eu>
2024-03-30 05:28:47 +0100

    fix: infinite scroll on /list + skeleton

--- app/list/InvaderList.tsx
+++ app/list/InvaderList.tsx
@@ -1,32 +1,47 @@
 "use client";
+import InvaderHit from "@/app/list/InvaderHit";
+import { SkeletonHit } from "@/app/list/SkeletonHit";
 import { Invader } from "@/db";
-import React from "react";
-import InfiniteScroll from "react-infinite-scroller";
+import { Colors } from "@/utils";
+import React, { useState } from "react";
+import useInfiniteScroll from "react-infinite-scroll-hook";
 import {
   useInfiniteHits,
   UseInfiniteHitsProps,
+  useInstantSearch,
 } from "react-instantsearch-hooks";
-import InvaderHit from "@/app/list/InvaderHit";
-import { Colors } from "@/utils";
 
 const InvaderList = (props: UseInfiniteHitsProps<Invader>) => {
   const { hits, isLastPage, showMore, results } = useInfiniteHits(props);
+  const { status } = useInstantSearch();
+  const loading = status === "stalled" || status === "loading";
+  const [triggered, setTriggered] = useState(false);
+
+  const [sentryRef, { rootRef }] = useInfiniteScroll({
+    loading: loading,
+    hasNextPage: !isLastPage,
+    onLoadMore: showMore,
+    disabled: isLastPage || status === "error",
+    rootMargin: "0px 0px 400px 0px",
+  });
+
   return (
     <div
       className="scrollbar carousel-vertical h-full scroll-pt-4 overflow-y-scroll scrollbar-thumb-current scrollbar-track-black"
       style={{ scrollbarColor: Colors.primary }}
+      ref={rootRef}
     >
-      <InfiniteScroll
-        className="grid grid-cols-1 gap-4 p-2 sm:grid-cols-2 md:grid-cols-3 md:p-4 lg:p-6 xl:grid-cols-4"
-        loadMore={showMore || (() => null)}
-        hasMore={!isLastPage}
-        useWindow={false}
-      >
+      <div className="grid grid-cols-1 gap-4 p-2 sm:grid-cols-2 md:grid-cols-3 md:p-4 lg:p-6 xl:grid-cols-4">
         {hits.map((hit) => (
-          <InvaderHit key={hit.objectID} {...hit} />
+          <InvaderHit key={hit.id} {...hit} />
         ))}
-      </InfiniteScroll>
+        {(loading || triggered) &&
+          !isLastPage &&
+          [...Array(20)].map((_x, i) => <SkeletonHit key={i} />)}
+      </div>
+      {(loading || !isLastPage) && <div ref={sentryRef}></div>}
     </div>
+    // </>
   );
 };
 
