e8e6b8bedcdf104b492b878d40c25c0304c6dc4b
v1s10n_4 <romain.daniel@epitech.eu>
2024-03-31 04:20:04 +0200

    chore: add Shimmer component for skeleton

--- components/Shimmer.tsx
+++ components/Shimmer.tsx
@@ -1,8 +1,9 @@
 import { toBase64 } from "@/utils";
+import { FC, HTMLProps } from "react";
 
 export const Shimmer = (w: number, h: number) =>
   toBase64(`
-<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <defs>
     <linearGradient id="g">
       <stop stop-color="#333" offset="20%" />
@@ -13,4 +14,37 @@ export const Shimmer = (w: number, h: number) =>
   <rect width="${w}" height="${h}" fill="#333" />
   <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
   <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-</svg>`);
+</svg>
+`);
+
+/* duplicated from Shimmer because we also need it as string and we don't want react to render it */
+export const ShimmerBase64: FC<HTMLProps<SVGElement>> = ({
+  width: w,
+  height: h,
+}) => (
+  <svg
+    width={w}
+    height={h}
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+  >
+    <defs>
+      <linearGradient id="g">
+        <stop stopColor="#333" offset="20%" />
+        <stop stopColor="#222" offset="50%" />
+        <stop stopColor="#333" offset="70%" />
+      </linearGradient>
+    </defs>
+    <rect width={w} height={h} fill="#333" />
+    <rect id="r" width={w} height={h} fill="url(#g)" />
+    <animate
+      xlinkHref="#r"
+      attributeName="x"
+      from={`-${w}`}
+      to={w}
+      dur="1s"
+      repeatCount="indefinite"
+    />
+  </svg>
+);
