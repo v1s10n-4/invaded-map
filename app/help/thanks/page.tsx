d859d9ac19bb1b9965ed542428a70d9dcdd82a4c
v1s10n_4 <romain.daniel@epitech.eu>
2024-08-21 17:37:13 +0200

    feat(UI): migrate help/thanks page to radix theme

--- app/help/thanks/page.tsx
+++ app/help/thanks/page.tsx
@@ -1,29 +1,43 @@
 import DiscordIcon from "@/public/icons/discord-logo.svg";
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Heading,
+  Section,
+  Text,
+} from "@radix-ui/themes";
 
 export const runtime = "edge";
 
 const DISCORD_INVITE_LINK = process.env.NEXT_PUBLIC_DISCORD_INVITE_LINK!;
 const FeedbackPage = () => (
-  <main className="mx-auto flex h-full flex-col items-center gap-8 bg-black px-4 pb-8 pt-32 text-primary md:max-w-screen-md md:px-12">
-    <h4 className="text-center text-2xl uppercase md:mt-12 md:text-3xl">
-      Thanks for your message!
-    </h4>
-    <p className="md:text-md text-sm">
-      Your feedback matters and helps us grow. Join our Discord to contribute to
-      our street art community website&apos;s growth!
-    </p>
-    <p className="text-xs">
-      Your input, ideas, and passion are what fuel our progress. Let&apos;s
-      collaborate to create the ultimate hub for street art enthusiasts.
-      Together, we&apos;ll shape the future of this website!
-    </p>
-    <a
-      href={DISCORD_INVITE_LINK}
-      className="flex items-center gap-2 border-2 border-dashed border-primary px-6 py-2 text-2xl outline-none"
-    >
-      <DiscordIcon className="h-16 w-16" /> Discord
-    </a>
-  </main>
+  <Container size="1" asChild>
+    <main>
+      <Section size="4" px={{ initial: "2", sm: "0" }}>
+        <Flex direction="column">
+          <Heading mb="4" align="center">
+            Thanks for your message!
+          </Heading>
+          <Text size="2" mb="4" align="center">
+            Your feedback matters and helps us grow. Join our Discord to
+            contribute to our street art community website&apos;s growth!
+          </Text>
+          <Text size="1" mb="8" align="center">
+            Your input, ideas, and passion are what fuel our progress.
+            Let&apos;s collaborate to create the ultimate hub for street art
+            enthusiasts. Together, we&apos;ll shape the future of this website!
+          </Text>
+          <Button asChild size="4" variant="outline" mx="auto">
+            <a href={DISCORD_INVITE_LINK}>
+              <DiscordIcon className="h-8 w-8" /> Discord
+            </a>
+          </Button>
+        </Flex>
+      </Section>
+    </main>
+  </Container>
 );
 
 export default FeedbackPage;
