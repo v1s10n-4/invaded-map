66fb5bb4a922a8b8893bef0fa45dcecb9c8d658b
v1s10n_4 <romain.daniel@epitech.eu>
2024-08-31 21:00:36 +0200

    chore(UI): use radix-theme for SliderActions

--- components/SliderActions.tsx
+++ components/SliderActions.tsx
@@ -1,17 +1,17 @@
 import { CarouselNext, CarouselPrevious } from "@/components/Carousel";
+import { IconButton } from "@radix-ui/themes";
 import ChevronLeft from "pixelarticons/svg/chevron-left.svg";
 import ChevronRight from "pixelarticons/svg/chevron-right.svg";
 import React, { FC } from "react";
 import { clsx } from "clsx";
 
-const SliderActionClasses = clsx("btn btn-square btn-outline bg-black/70");
 export const SliderActions: FC = () => {
   return (
     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-      <CarouselPrevious className={SliderActionClasses}>
+      <CarouselPrevious color="gray" variant="surface" size="3">
         <ChevronLeft className="h-full w-full" />
       </CarouselPrevious>
-      <CarouselNext className={SliderActionClasses}>
+      <CarouselNext color="gray" variant="surface" size="3">
         <ChevronRight className="h-full w-full" />
       </CarouselNext>
     </div>
