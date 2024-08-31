0af3d2cad3024ffa5adfd53ddaa6a4c42bd680ac
v1s10n_4 <romain.daniel@epitech.eu>
2024-08-31 21:01:12 +0200

    feat(UI): use radix-theme in MapSheet

--- components/MapSheet.tsx
+++ components/MapSheet.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Flex, Heading, IconButton, Text, Theme } from "@radix-ui/themes";
 import Sheet, { SheetRef } from "react-modal-sheet";
 import React, {
   FC,
@@ -14,15 +15,6 @@ import Close from "pixelarticons/svg/close.svg";
 import ChevronDown from "pixelarticons/svg/chevron-down.svg";
 import ChevronUp from "pixelarticons/svg/chevron-up.svg";
 import ArrowLeft from "pixelarticons/svg/arrow-left.svg";
-import { clsx } from "clsx";
-
-const SheetActionClassNames = clsx(
-  "h-full w-10 bg-base-100 p-1 box-border",
-  "hover:text-primary hover:ring-1 hover:ring-primary",
-  "focus-visible:text-primary focus-visible:ring-1 focus-visible:ring-primary"
-);
-
-const SheetHeaderTextClassNames = "px-4 py-2 md:px-8";
 
 export const MapSheet: FC<PropsWithChildren> = ({ children }) => {
   const router = useRouter();
@@ -73,65 +65,67 @@ export const MapSheet: FC<PropsWithChildren> = ({ children }) => {
         position: "absolute",
       }}
     >
-      <Sheet.Container className="!md:inset-x-4 !md:bottom-4 !inset-x-2 !bottom-2 !w-auto !overflow-hidden !rounded-none border-4 border-double border-primary !bg-base-100">
-        <Sheet.Header className="relative mb-0.5 flex h-12 items-center justify-between border-b border-primary bg-black p-[3px]">
-          <button onClick={onLeftActionClick} className={SheetActionClassNames}>
-            {invaderName ? (
-              <ArrowLeft className="h-full w-full" />
-            ) : currentSnapPoint === MapSheetState.FULL ? (
-              <ChevronDown className="h-full w-full" />
-            ) : (
-              <ChevronUp className="h-full w-full" />
-            )}
-          </button>
-          {invaderName ? (
-            <h1
-              onClick={() =>
-                sheetRef.current?.snapTo(
-                  currentSnapPoint === MapSheetState.CLOSED
-                    ? MapSheetState.FULL
-                    : MapSheetState.MID
-                )
-              }
-              className={clsx(SheetHeaderTextClassNames, "text-2xl")}
-            >
-              {invaderName}
-            </h1>
-          ) : (
-            <p
-              className={SheetHeaderTextClassNames}
-              onClick={() =>
-                sheetRef.current?.snapTo(
-                  currentSnapPoint === MapSheetState.MID
-                    ? MapSheetState.MIN
-                    : MapSheetState.MID
-                )
-              }
-            >
-              {invadersAmount} found
-            </p>
-          )}
-          <button
-            onClick={() => sheetRef.current?.snapTo(MapSheetState.CLOSED)}
-            className={clsx(
-              SheetActionClassNames,
-              currentSnapPoint === MapSheetState.CLOSED && "opacity-0"
-            )}
-          >
-            <Close className="h-full w-full" />
-          </button>
-        </Sheet.Header>
-        <Sheet.Content
-          className="scrollbar border-t border-primary"
-          disableDrag={
-            !!invaderName ||
-            currentSnapPoint === MapSheetState.MIN ||
-            currentSnapPoint === MapSheetState.MID
-          }
+      <Theme>
+        <Sheet.Container
+          className="!md:inset-x-4 !md:bottom-4 rt-reset rt-BaseCard rt-Card rt-r-size-1 rt-variant-surface rt-Card-accent !inset-x-2 !bottom-2 !w-auto !overflow-hidden"
+          style={{
+            backgroundColor: "inherit",
+            borderTopRightRadius: "initial",
+            borderTopLeftRadius: "initial",
+            boxShadow: "initial",
+          }}
         >
-          {children}
-        </Sheet.Content>
-      </Sheet.Container>
+          <Sheet.Header>
+            <Flex justify="between" px="3" pt="3" pb="1" align="center">
+              <IconButton variant="ghost" size="3" onClick={onLeftActionClick}>
+                {invaderName ? (
+                  <ArrowLeft className="h-8 w-8" />
+                ) : currentSnapPoint === MapSheetState.FULL ? (
+                  <ChevronDown className="h-8 w-8" />
+                ) : (
+                  <ChevronUp className="h-8 w-8" />
+                )}
+              </IconButton>
+              {invaderName ? (
+                <Heading
+                  onClick={() =>
+                    sheetRef.current?.snapTo(
+                      currentSnapPoint === MapSheetState.CLOSED
+                        ? MapSheetState.FULL
+                        : MapSheetState.MID
+                    )
+                  }
+                >
+                  {invaderName}
+                </Heading>
+              ) : (
+                <Text
+                  onClick={() =>
+                    sheetRef.current?.snapTo(
+                      currentSnapPoint === MapSheetState.MID
+                        ? MapSheetState.MIN
+                        : MapSheetState.MID
+                    )
+                  }
+                >
+                  {invadersAmount} found
+                </Text>
+              )}
+              <IconButton
+                variant="ghost"
+                size="3"
+                onClick={() => sheetRef.current?.snapTo(MapSheetState.CLOSED)}
+                className={
+                  currentSnapPoint === MapSheetState.CLOSED ? "opacity-0" : ""
+                }
+              >
+                <Close className="h-8 w-8" />
+              </IconButton>
+            </Flex>
+          </Sheet.Header>
+          <Sheet.Content>{children}</Sheet.Content>
+        </Sheet.Container>
+      </Theme>
     </Sheet>
   ) : (
     <div className="sr-only">{children}</div>
