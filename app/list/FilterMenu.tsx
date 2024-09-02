9e768e4a3e809054d477a52a5310616f394ae029
v1s10n_4 <romain.daniel@epitech.eu>
2024-09-02 16:39:05 +0200

    feat(UI): migrate /list MenuBar to radix-themes

--- app/list/FilterMenu.tsx
+++ app/list/FilterMenu.tsx
@@ -3,7 +3,7 @@ import { MenuBar } from "@/components/MenuBar";
 
 export const FilterMenu = () => {
   return (
-    <MenuBar className="h-full w-full border-[--gray-a7] p-1.5">
+    <MenuBar className="h-full w-full rounded-[max(var(--radius-3),var(--radius-full))] p-2 px-3.5 after:rounded-[max(var(--radius-3),var(--radius-full))]">
       <Filter attribute="city" limit={Infinity} />
       <Filter attribute="points" />
       <Filter attribute="state" />
