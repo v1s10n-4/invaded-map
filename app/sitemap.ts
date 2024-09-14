b5577d495e76810824bd48abc5c82b901e7eae1b
v1s10n_4 <romain.daniel@epitech.eu>
2024-09-14 21:24:23 +0200

    fix: use drizzle directly for sitemap generation + handle errors

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,33 +1,43 @@
-import { getInvaders } from "@/utils/data";
 import { MetadataRoute } from "next";
+import { db } from "@/db";
+import { invaders as invadersTable } from "@/db/schema/invaders";
 
 const host = process.env.URL!;
+
+const baseSitemap = [
+  {
+    url: `${host}`,
+    lastModified: new Date(),
+  },
+  {
+    url: `${host}/map`,
+    lastModified: new Date(),
+  },
+  {
+    url: `${host}/list`,
+    lastModified: new Date(),
+  },
+  {
+    url: `${host}/help`,
+    lastModified: new Date(),
+  },
+  {
+    url: `${host}/highscores`,
+    lastModified: new Date(),
+  },
+];
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const invaders = await getInvaders();
-  return [
-    {
-      url: `${host}`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${host}/map`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${host}/list`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${host}/help`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${host}/highscores`,
-      lastModified: new Date(),
-    },
-    ...invaders.map((invader) => ({
-      url: `${host}/map/${invader.name}`,
-      lastModified: new Date(),
-    })),
-  ];
+  try {
+    const invaders = await db.select().from(invadersTable);
+    return [
+      ...invaders.map((invader) => ({
+        url: `${host}/map/${invader.name}`,
+        lastModified: new Date(),
+      })),
+    ];
+  } catch (err) {
+    console.error("error while generating sitemap", err);
+    return baseSitemap;
+  }
 }
